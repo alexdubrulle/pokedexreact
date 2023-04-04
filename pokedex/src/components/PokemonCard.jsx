bfdc36b46084b984da898c40596a4e2d2223b4a1
Alex DUBRULLE--FAGNONI <alexdubrullefagnoni26@hotmail.fr>
2023-04-04 16:39:23 +0200

    if img

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    let pokemon = pokemonList[1]
     return (
         <div>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-                <figcaption>bulbasaur</figcaption>
+                {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+                <figcaption>{pokemon.name}</figcaption>
             </figure>
         </div>
     );
