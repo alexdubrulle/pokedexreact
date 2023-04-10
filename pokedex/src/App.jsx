709ba091a873e712f71f0abb405668c70c9fdb4a
Alex DUBRULLE--FAGNONI <alexdubrullefagnoni26@hotmail.fr>
2023-04-10 11:26:34 +0200

    use state

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,33 +1,51 @@
+import { useState } from 'react'
 import './App.css'
 import './components/PokemonCard'
 import PokemonCard from './components/PokemonCard'
-import PropTypes from "prop-types";
 
 
 function App() {
-  
-  const pokemonList = [{
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
   ];
-   
-  PokemonCard.propTypes = {
-    pokemon : PropTypes.shape({
-    name : PropTypes.string.isRequired,
-    imgSrc : PropTypes.string
-    }).isRequired,
-    }
- 
- return (<div>
-   
-      <PokemonCard pokemon={pokemonList[0]} />
-  
-      </div>
+
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const clickPrevious = () => { (pokemonIndex > 0 ? setPokemonIndex(pokemonIndex - 1) : "") }
+
+  const clickNext = () => { (pokemonIndex < pokemonList.length - 1 ? setPokemonIndex(pokemonIndex + 1) : "") }
+
+  return (<div>
+
+    <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+    <button onClick={clickPrevious}>Previous</button>
+    <button onClick={clickNext}>Next</button>
+
+  </div>
   );
 
 }
--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function PokemonCard(props) {
     console.log(props)
     return (
